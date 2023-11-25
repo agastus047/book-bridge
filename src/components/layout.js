12785341ff929ab3e10239e98fc8a3a4ef0a54c4
Agastus Joyson <agastusjoyson@gmail.com>
2023-11-25 23:31:51 +0530

    set user context

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,8 +1,48 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import Head from "next/head";
+import { useSession } from "next-auth/react";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import { useContext } from "react";
+import { UserContext } from "@/contexts/UserContext";
 
 export default function Layout({children}) {
+  const {status} = useSession();
+  const router = useRouter();
+
+  const {userState} = useContext(UserContext);
+  const [userDetails, setUserDetails] = userState;
+  
+  const getData = async () => {
+    const user = await fetch('/api/user',{
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    });
+    return user.json();
+  };
+
+  useEffect(() => {
+    (async () => {
+      if(status==="authenticated") {
+        if(!userDetails) {
+          const response = await getData();
+          if(response.user) {
+            setUserDetails({...response.user});
+          }
+          if(response.user.isProfileCompleted===false)
+            router.push("/edit_profile");
+        }
+      }
+      else if(status==="unauthenticated") {
+        setUserDetails(null);
+        router.push("/");
+      }
+    })();
+  },[status]);
+
     return(<>
         <Head>
             <title>Book Bridge</title>
---
+++ src/pages/api/user.js
@@ -0,0 +1,28 @@
+import prisma from "../../../lib/prisma";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./auth/[...nextauth]";
+
+export default async function handler(req, res) {
+  const session = await getServerSession(req,res,authOptions);
+  if(req.method === 'GET') {
+    if(session) {
+      try {
+        const user = await prisma.user.findUnique({
+          where: {
+            email: session.user.email,
+          }
+        });
+        console.log("db user fetched");
+        res.status(200).json({user});
+      }
+      catch(err) {
+        res.status(500).json({error:"An error occured"});
+      }
+    }
+    else {
+      res.status(401).json({error: "unauthenticated"});
+    }
+  } else {
+    res.status(405).json({ error: "Method not allowed" });
+  }
+}

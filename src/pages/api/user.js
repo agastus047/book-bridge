1757d527445759f4a016a73969fa2bcbde8061a3
Agastus Joyson <agastusjoyson@gmail.com>
2023-11-26 00:37:15 +0530

    complete edit profile

--- src/pages/api/user.js
+++ src/pages/api/user.js
@@ -22,7 +22,28 @@ export default async function handler(req, res) {
     else {
       res.status(401).json({error: "unauthenticated"});
     }
-  } else {
+  }
+  else if(req.method === 'POST') {
+    if(session) {
+      try {
+        const user = await prisma.user.update({
+          where: {
+            email: session.user.email,
+          },
+          data: req.body,
+        });
+        console.log('user updated');
+        res.status(200).json({user});
+      }
+      catch(err) {
+        res.status(500).json({error:"An error occured"});
+      }
+    }
+    else {
+      res.status(401).json({error: "unauthenticated"});
+    }
+  }
+  else {
     res.status(405).json({ error: "Method not allowed" });
   }
 }

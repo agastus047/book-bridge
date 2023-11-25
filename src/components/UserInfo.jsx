aa56c69aa1c29888b89ea49a86578061cfe42de5
Agastus Joyson <agastusjoyson@gmail.com>
2023-11-25 23:40:17 +0530

    style home page

--- src/components/UserInfo.jsx
+++ src/components/UserInfo.jsx
@@ -4,7 +4,6 @@ import { signIn } from "next-auth/react";
 
 export default function UserInfo() {
   const { status, data: session } = useSession();
-  //console.log(session);
   if (status === "authenticated") {
     return (
       <div className="shadow-xl p-8 rounded-md flex flex-col gap-3 bg-yellow-200">
@@ -25,12 +24,14 @@ export default function UserInfo() {
     );
   } else {
     return (
+      <div className="flex justify-center mt-20">
         <button onClick={() => signIn("google")}
-        className="flex items-center gap-4 shadow-xl rounded-lg pl-3">
+        className="flex items-center gap-4 shadow-xl">
             <span className="bg-blue-500 text-white px-4 py-3">
                 Sign in with Google
             </span>
         </button>
+      </div>
     );
   }
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ import UserInfo from "@/components/UserInfo"
 export default function Home() {
   return (
     <main>
-      <div>Home Page</div>
       <UserInfo/>
     </main>
   )
